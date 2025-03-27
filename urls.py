a3ef1685020576744bcbdd056c454c7cc6852599
RaniSrd <ranirb183@gmail.com>
2025-03-27 09:59:31 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -2,5 +2,5 @@ from django.urls import path # type: ignore
 from .views import appointment
 
 urlpatterns = [
-    path('appointment/', appointment, name='appointment'),
+    path('/', appointment, name='index'),
 ]
